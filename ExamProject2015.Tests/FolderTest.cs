4daa1e70f919eb8dc319a1ab0534f07da8d4f33c
NinjaBobxD <c.nielsen39@gmail.com>
2015-05-21 12:43:51 +0200

    dsafer

---
+++ ExamProject2015.Tests/FolderTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExamProject2015.Tests
+{
+    [TestClass]
+    public class FolderTest
+    {
+        [TestMethod]
+        public void CreateFolderTest()
+        {
+            MainController c = new MainController();
+            bool answer = c.CreateFolder("Test123", 123);
+
+            Assert.AreEqual(true, answer);
+
+        }
+
+        [TestMethod]
+        public void RenemaeFolderTest()
+        {
+        }
+
+        [TestMethod]
+        public void DeleteFolderTest()
+        {
+        }
+    }
+}
