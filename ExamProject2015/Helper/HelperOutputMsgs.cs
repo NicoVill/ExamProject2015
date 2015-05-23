c770678649332807cc64bff9972808b8e13d8caa
NicoVill <o_sl_f@hotmail.com>
2015-05-23 17:34:55 +0200

    Message helper

---
+++ ExamProject2015/Helper/HelperOutputMsgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamProject2015
+{
+    public static class HelperOutputMsgs
+    {
+        public static string printMessage(int nr)
+        {
+            switch (nr)
+            {
+                case -1:
+                    return "Navnet er allerede brugt";
+                    
+
+                case 1:
+                    return "Filen er oploaded";
+                    
+
+                case 2:
+                    return "Mappen er oprettet";
+            }
+
+            return "Fejl i Message Helper";
+        }
+    }
+}
